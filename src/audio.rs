c2982724041dd06123f1ab9ad3d886e9319c560e
Olivier Pinon <oliv.pinon@gmail.com>
2020-04-04 14:01:41 +0200

    Implement AudioSystem

--- src/audio.rs
+++ src/audio.rs
@@ -60,3 +60,12 @@ pub fn play_bounce(sounds: &Sounds, storage: &AssetStorage<Source>, output: Opti
         }
     }
 }
+
+/// Plays the score sound when a player gains a point.
+pub fn play_score(sounds: &Sounds, storage: &AssetStorage<Source>, output: Option<&Output>) {
+    if let Some(ref output) = output.as_ref() {
+        if let Some(sound) = storage.get(&sounds.score_sfx) {
+            output.play_once(sound, 1.0);
+        }
+    }
+}
---
+++ src/systems/audio.rs
@@ -0,0 +1,46 @@
+use crate::audio::{play_bounce, play_score, Sounds};
+use crate::event::PongEvent;
+
+use amethyst::assets::AssetStorage;
+use amethyst::audio::{output::Output, Source};
+use amethyst::ecs::{Join, Read, ReadExpect, System, SystemData, World};
+use amethyst::shrev::{EventChannel, ReaderId};
+
+use std::ops::Deref;
+
+pub struct AudioSystem {
+    pong_event_reader: Option<ReaderId<PongEvent>>,
+}
+
+impl<'s> System<'s> for AudioSystem {
+    type SystemData = (
+        Read<'s, AssetStorage<Source>>,
+        ReadExpect<'s, Sounds>,
+        Option<Read<'s, Output>>,
+        Read<'s, EventChannel<PongEvent>>,
+    );
+
+    fn run(&mut self, (storage, sounds, audio_output, pong_events): Self::SystemData) {
+        // Reads PongEvent, play sound accordingly
+        let reader = self
+            .pong_event_reader
+            .as_mut()
+            .expect("AudioSystem::setup has not been called");
+
+        pong_events
+            .read(reader)
+            .for_each(|ev| match ev {
+                PongEvent::Bounce => {
+                    play_bounce(&*sounds, &storage, audio_output.as_ref().map(|o| o.deref()))
+                }
+                PongEvent::Score => {
+                    play_score(&*sounds, &storage, audio_output.as_ref().map(|o| o.deref()))
+                }
+            });
+    }
+
+    fn setup(&mut self, world: &mut World) {
+        Self::SystemData::setup(world); 
+        self.pong_event_reader = Some(world.fetch_mut::<EventChannel<PongEvent>>().register_reader());
+    }
+}
